e0587bd7db2939825acbd2add2fca28786a238ee
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-28 10:18:56 -0500

    Add tweet and user api

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
 mongoose
-	.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-	.then(() => console.log("Connected to MongoDB successfully"));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Test Routes
+app.get("/", (req, res) => res.send("Jello Squirell"));
+app.use("/api/tweets", tweets);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
