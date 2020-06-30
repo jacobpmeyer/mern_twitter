4859e5db433e66c686b4520caec0d7496267a8c6
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-06-30 11:17:06 -0500

    Edit tweet schema

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-router.get("/test", (req, res) =>
-  res.json({ msg: "This is the tweets route" }),
-);
+const Tweet = require("../../models/tweets");
+const validateTweetInput = require("../../validation/tweets");
+
+router.get("/", (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then(tweets => res.json(tweets))
+    .catch(err => res.status(404).json({ notweetsfound: "No Tweets found" }));
+});
+
+router.get("/user/:user_id", (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .then(tweets => res.json(tweets))
+    .catch(err =>
+      res.status(404).json({ notweetsfound: "No tweets from this user found" }),
+    );
+});
 
 module.exports = router;
