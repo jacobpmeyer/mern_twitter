3adf0dda7c7d3f720a9d9336377a343454e3eeb2
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-10 16:57:32 -0500

    Fix fat arrow in export default

--- frontend/src/reducers/session_api_reducer.js
+++ frontend/src/reducers/session_api_reducer.js
@@ -1,33 +1,33 @@
 import {
-	RECEIVE_CURRENT_USER,
-	RECEIVE_USER_LOGOUT,
-	RECEIVE_USER_SIGN_IN,
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_USER_SIGN_IN,
 } from "../actions/session_actions";
 
 const initialState = {
-	isAuthenticated: false,
-	user: {},
+  isAuthenticated: false,
+  user: {},
 };
 
-export default (state = initialState, actin) {
-	switch (applicationCache.type) {
-		case RECEIVE_CURRENT_USER:
-			return {
-				...state,
-				isAuthenticated: !!action.currentUser,
-				user: action.currentUser,
-			};
-		case RECEIVE_USER_LOGOUT:
-			return {
-				isAuthenticated: false,
-				user: undefined,
-			};
-		case RECEIVE_USER_SIGN_IN:
-			return {
-				...state, 
-				isSignedIn: true,
-			};
-		default: 
-		return state;
-	};
+export default (state = initialState, action) => {
+  switch (applicationCache.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser,
+      };
+    case RECEIVE_USER_LOGOUT:
+      return {
+        isAuthenticated: false,
+        user: undefined,
+      };
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true,
+      };
+    default:
+      return state;
+  }
 };
