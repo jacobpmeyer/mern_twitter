d0dbef310ccc042af94103c6248f9bc17111f74b
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-10 16:51:25 -0500

    Add signup and login to session api util

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -7,3 +7,11 @@ export const setAuthToken = token => {
     delete axios.defaults.headers.commoin["Authorization"];
   }
 };
+
+export const signup = userData => {
+  return axios.post("/api/users/register", userData);
+};
+
+export const login = userData => {
+  return axios.post("/api/user/login", userData);
+};
