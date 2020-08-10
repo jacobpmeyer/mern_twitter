150447878b3bfd64b41a4ae4b9644dbe92271cc1
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-08-10 14:34:45 -0500

    Create fronted protected and auth routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import Root from "./components/root";
+import configureStore from "./store/store";
+import jwt_decode from "jwt-decode";
+import { setAuthToken } from "./util/session_api_util";
+import { logout } from "./actions/session_actions";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = {
+      session: {
+        isAuthenticated: true,
+        user: decodedUser,
+      },
+    };
+  }
+});
